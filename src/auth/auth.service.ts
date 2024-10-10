51150b121e1dd721941165b65db7faf108ebf093
gabrielolivra <gabriel.bernardino@urca.br>
2024-10-10 15:37:05 -0300

    criptografia de senha

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
     constructor(
@@ -14,9 +14,12 @@ export class AuthService {
         pass: string,
     ): Promise<{ access_token: string }> {
         const user = await this.usersService.findOneEmail(email);
-        if (user?.password !== pass) {
+
+        const isPasswordValid = user && await bcrypt.compare(pass, user.password);
+        if (!isPasswordValid) {
             throw new UnauthorizedException();
         }
+
         const payload = { sub: user.id, email: user.email };
         return {
             access_token: await this.jwtService.signAsync(payload),
